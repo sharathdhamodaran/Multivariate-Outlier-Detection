298d51e70a58c806d650e08db5c23b0260fa3879
Sharath Kumar Dhamodaran <sharath.1160@gmail.com>
2020-05-10 14:36:52 -0500

    Create libraries.R
    added project files

---
+++ scripts/libraries.R
@@ -0,0 +1,19 @@
+# load the libraries -----------------------------------------------------------
+library(tidyverse)  # data manipulation
+library(tidyr)  # data manipulation
+library(gdata)  # Data Manipulation
+library(ggplot2)  # data Visualisation
+library(ggrepel)  # help avoid overlapping text labels
+library(gridExtra) # adds custom table inside ggplot
+library(scales)  # breaks and labels for axes and legends 
+library(ggthemes)  # Adding desired ggplot themes
+library(tseries)  # time series package
+library(tidyquant)  # financial time series
+library(here)  #  tell R where the data is
+library(viridis)  # color palette
+library(data.table)
+library(bit64)
+library(pheatmap)
+library(RColorBrewer)
+library(reshape2)
+library(caTools)  # loading caTools library
